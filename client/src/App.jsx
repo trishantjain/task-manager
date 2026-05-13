58734ac53e733861d81f564a6968283e046be1ff
trishantjain <trishantjain4444@gmail.com>
2026-05-13 23:14:53 +0530

    Initialized React frontend with Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
+      Team Task Manager
+    </div>
+  );
+}
+
+export default App;
